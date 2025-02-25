7e77037b881fa4831536a28ce35eb0bffd4a31cd
Jessica Aguiar <jessicaag.rs@gmail.com>
2025-02-25 10:40:02 -0300

    feat: updates 404 error page with new layout and components

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,15 @@
+import Button from '@/components/globals/Button';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="h-screen flex flex-col gap-5 items-center justify-center">
+      <h2>Página não encontrada</h2>
+      <Image src="/Attention.svg" width={150} height={150} alt="Error" />
+      <Button width="w-40">
+        <Link href="/">Voltar ao início</Link>
+      </Button>
     </div>
   );
 }
